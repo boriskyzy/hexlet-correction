bbceb95e76b929da1bc8639dcd5132b07cf77110
Artem Gorbounov <artem.gorbounov@neotys.com>
2020-06-03 17:53:23 +0200

    add TypoApi for receiving a typo report

---
+++ src/main/java/io/hexlet/hexletcorrection/web/Routers.java
@@ -0,0 +1,10 @@
+package io.hexlet.hexletcorrection.web;
+
+public class Routers {
+
+    static final String API = "/api";
+
+    static final String TYPOS = "/typos";
+
+    static final String API_TYPOS = API + TYPOS;
+}
---
+++ src/main/java/io/hexlet/hexletcorrection/web/TypoApi.java
@@ -0,0 +1,31 @@
+package io.hexlet.hexletcorrection.web;
+
+import io.hexlet.hexletcorrection.domain.Typo;
+import io.hexlet.hexletcorrection.service.TypoService;
+import io.hexlet.hexletcorrection.web.vm.TypoReport;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static io.hexlet.hexletcorrection.web.Routers.API_TYPOS;
+import static org.springframework.http.ResponseEntity.created;
+
+@Slf4j
+@RestController
+@RequestMapping(API_TYPOS)
+@RequiredArgsConstructor
+public class TypoApi {
+
+    private final TypoService service;
+
+    @PostMapping
+    public ResponseEntity<Typo> getAllTypos(@Valid @RequestBody TypoReport report) throws URISyntaxException {
+        final var savedTypo = service.addTypoReport(report);
+        return created(new URI("/typos/" + savedTypo.getId())).body(savedTypo);
+    }
+}
